545979f7d54705f64328ed449362bbdd2f567388
vandosant <sjskender@gmail.com>
2014-07-28 18:44:02 -0600

    Refactor to extract constructor pattern

---
+++ public/javascript/oscillator.js
@@ -0,0 +1,58 @@
+function Oscillator(context, frequency, volume) {
+  this.context = context;
+  this.osc1 = this.context.createOscillator();
+  this.osc2 = this.context.createOscillator();
+  this.osc3 = this.context.createOscillator();
+  this.gainNode1 = context.createGain();
+  this.gainNode2 = context.createGain();
+  this.gainNode3 = context.createGain();
+  this.frequency = frequency;
+  this.volume = volume;
+}
+
+Oscillator.prototype.connect = function () {
+  this.gainNode1.connect(this.context.destination);
+  this.gainNode1.gain.value = this.volume;
+  this.osc1.connect(this.gainNode1);
+  this.osc1.type = 0;
+  this.osc1.frequency.value = this.frequency;
+  this.osc1.start(0);
+
+  this.gainNode2.connect(this.context.destination);
+  this.gainNode2.gain.value = this.volume;
+  this.osc2.connect(this.gainNode2);
+  this.osc2.type = 1;
+  this.osc2.frequency.value = this.frequency / 2;
+  this.osc2.start(0);
+
+  this.gainNode3.connect(this.context.destination);
+  this.gainNode3.gain.value = this.volume * 0.8;
+  this.osc3.connect(this.gainNode3);
+  this.osc3.type = 3;
+  this.osc3.frequency.value = this.frequency / 2;
+  this.osc3.start(0);
+};
+
+Oscillator.prototype.disconnect = function () {
+  this.osc1.disconnect();
+  this.osc2.disconnect();
+  this.osc3.disconnect();
+};
+
+Oscillator.prototype.updateVolume = function (value) {
+  this.gainNode1.gain.value = (value / 100);
+  this.gainNode2.gain.value = (value / 100);
+  this.gainNode3.gain.value = (value / 100) * 0.8;
+};
+
+Oscillator.prototype.updateFrequency = function (value) {
+  this.osc1.frequency.value = (value * 100);
+  this.osc2.frequency.value = (value * 100) / 2;
+  this.osc3.frequency.value = (value * 100) / 2;
+};
+
+Oscillator.prototype.updateNote = function (value) {
+  this.osc1.frequency.value = (value);
+  this.osc2.frequency.value = (value) / 2;
+  this.osc3.frequency.value = (value) / 2;
+};
