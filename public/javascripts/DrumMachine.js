0118355707cd0c5c13ee7e0c6ac03be2f3b0f5bb
vandosant <sjskender@gmail.com>
2014-08-06 16:56:47 -0600

    Implemented a solid-state kick drum

--- public/javascripts/DrumMachine.js
+++ public/javascripts/DrumMachine.js
@@ -1,8 +1,16 @@
 function DrumMachine(context, initialVolume) {
-  this.volume = initialVolume;
   this.context = context;
   this.bassDrum = this.context.createOscillator();
-  this.bassDrum.frequency.value = 16.35;
   this.bassNode = context.createGain();
   this.bassNode.gain.value = initialVolume;
+  this.volume = initialVolume;
 }
+
+DrumMachine.prototype.bassKick = function() {
+  this.bassNode.gain.value = 0.8;
+  this.bassNode.connect(this.context.destination);
+  this.bassDrum.connect(this.bassNode);
+  this.bassDrum.frequency.value = 52;
+  this.bassDrum.type = "sine";
+  this.bassDrum.start(0);
+};
