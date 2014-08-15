089796dacefc4706f99dede1196970595fc5d35b
vandosant <sjskender@gmail.com>
2014-08-15 14:43:33 -0600

    Editing drum sounds
    * Added octave-up echo
    * Addded tom and pad sounds
    * Tweaked all sounds for consistency

--- public/javascripts/DrumMachine.js
+++ public/javascripts/DrumMachine.js
@@ -22,6 +22,27 @@ DrumMachine.prototype.hit = function () {
   var fadeTimeout = setInterval(function () {
     if (node.gain.value > 0) {
       node.gain.value -= sustain;
+    } else {
+      drum.stop();
+    }
+  }, 5);
+
+  var drum1 = this.context.createOscillator();
+  var node2 = this.context.createGain();
+  var sustain1 = this.sustain;
+
+  node2.gain.value = this.gainVal * 0.8;
+  node2.connect(this.context.destination);
+  drum1.connect(node2);
+  drum1.frequency.value = this.frequency;
+  drum1.type = this.wave;
+  drum1.start(0);
+
+  var fadeTimeout = setInterval(function () {
+    if (node2.gain.value > 0) {
+      node2.gain.value -= sustain1 * 2;
+    } else {
+      drum1.stop();
     }
   }, 5);
 };
