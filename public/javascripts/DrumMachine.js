85b3d2712c05ca0e88d8c52c756b19e853b95415
vandosant <sjskender@gmail.com>
2014-08-28 12:13:07 -0600

    Refactoring DrumMachine for clarity

--- public/javascripts/DrumMachine.js
+++ public/javascripts/DrumMachine.js
@@ -4,45 +4,42 @@ function DrumMachine(context, frequency, wave, gainVal, sustain) {
   this.wave = wave;
   this.gainVal = gainVal;
   this.sustain = sustain;
-
 }
 
 DrumMachine.prototype.hit = function () {
-  var drum = this.context.createOscillator();
-  var node = this.context.createGain();
-  var sustain = this.sustain;
+  var drum1 = this.context.createOscillator(),
+      node1 = this.context.createGain(),
+      drum2 = this.context.createOscillator(),
+      node2 = this.context.createGain(),
+      sustain = this.sustain;
 
-  node.gain.value = this.gainVal;
-  node.connect(this.context.destination);
-  drum.connect(node);
-  drum.frequency.value = this.frequency;
-  drum.type = this.wave;
-  drum.start(0);
-
-  var fadeTimeout = setInterval(function () {
-    if (node.gain.value > 0) {
-      node.gain.value -= sustain;
-    } else {
-      drum.stop();
-    }
-  }, 5);
-
-  var drum1 = this.context.createOscillator();
-  var node2 = this.context.createGain();
-  var sustain1 = this.sustain;
-
-  node2.gain.value = this.gainVal * 0.8;
-  node2.connect(this.context.destination);
-  drum1.connect(node2);
+  node1.gain.value = this.gainVal;
+  node1.connect(this.context.destination);
+  drum1.connect(node1);
   drum1.frequency.value = this.frequency;
   drum1.type = this.wave;
   drum1.start(0);
 
-  var fadeTimeout = setInterval(function () {
-    if (node2.gain.value > 0) {
-      node2.gain.value -= sustain1 * 2;
+  setInterval(function () {
+    if (node1.gain.value > 0) {
+      node1.gain.value -= sustain;
     } else {
       drum1.stop();
+    }
+  }, 5);
+
+  node2.gain.value = this.gainVal * 0.8;
+  node2.connect(this.context.destination);
+  drum2.connect(node2);
+  drum2.frequency.value = this.frequency;
+  drum2.type = this.wave;
+  drum2.start(0);
+
+  setInterval(function () {
+    if (node2.gain.value > 0) {
+      node2.gain.value -= sustain * 2;
+    } else {
+      drum2.stop();
     }
   }, 5);
 };
