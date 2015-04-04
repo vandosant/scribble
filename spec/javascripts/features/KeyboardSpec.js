72d8fa9986fc77af13ba77a930422ccfb4514ef8
vandosant <sjskender@gmail.com>
2015-04-04 10:21:25 -0600

    Refactoring to move logic to Keyboard object

--- spec/javascripts/features/KeyboardSpec.js
+++ spec/javascripts/features/KeyboardSpec.js
@@ -12,5 +12,16 @@ describe("Keyboard", function() {
     keyDownEvent.keyCode = 65;
     $(document).trigger(keyDownEvent);
     expect(keyboard.keydown).toHaveBeenCalled();
-  })
+  });
+
+  it("should listen for keyup events", function(){
+    var keyboard = new Keyboard();
+    keyboard.listen();
+
+    spyOn(keyboard, "keyup");
+    var keyUpEvent = jQuery.Event("keyup");
+    keyUpEvent.keyCode = 65;
+    $(document).trigger(keyUpEvent);
+    expect(keyboard.keyup).toHaveBeenCalled();
+  });
 });
