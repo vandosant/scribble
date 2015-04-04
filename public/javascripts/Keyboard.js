f0ed965b2eac1ef7ea16e2310b3647bce0931afc
vandosant <sjskender@gmail.com>
2015-04-04 10:52:10 -0600

    Refactoring Keyboard to use revealing api

--- public/javascripts/Keyboard.js
+++ public/javascripts/Keyboard.js
@@ -1,106 +1,114 @@
-var keys = {
-  'A': {
-    key: 65,
-    index: 0,
-    freq: 261.63
-  },
-  'W': {
-    key: 87,
-    index: 1,
-    freq: 277.18
-  },
-  'S': {
-    key: 83,
-    index: 2,
-    freq: 293.66
-  },
-  'E': {
-    key: 69,
-    index: 3,
-    freq: 311.13
-  },
-  'D': {
-    key: 68,
-    index: 4,
-    freq: 329.63
-  },
-  'F': {
-    key: 70,
-    index: 5,
-    freq: 349.23
-  },
-  'T': {
-    key: 84,
-    index: 6,
-    freq: 369.99
-  },
-  'G': {
-    key: 71,
-    index: 7,
-    freq: 392.00
-  },
-  'Y': {
-    key: 89,
-    index: 8,
-    freq: 415.30
-  },
-  'H': {
-    key: 72,
-    index: 9,
-    freq: 440.00
-  },
-  'U': {
-    key: 85,
-    index: 10,
-    freq: 466.16
-  },
-  'J': {
-    key: 74,
-    index: 11,
-    freq: 493.88
-  },
-  'K': {
-    key: 75,
-    index: 12,
-    freq: 523.25
-  }
+var Keyboard = function () {
 };
+Keyboard.prototype = function () {
+  var keys = {
+    'A': {
+      key: 65,
+      index: 0,
+      freq: 261.63
+    },
+    'W': {
+      key: 87,
+      index: 1,
+      freq: 277.18
+    },
+    'S': {
+      key: 83,
+      index: 2,
+      freq: 293.66
+    },
+    'E': {
+      key: 69,
+      index: 3,
+      freq: 311.13
+    },
+    'D': {
+      key: 68,
+      index: 4,
+      freq: 329.63
+    },
+    'F': {
+      key: 70,
+      index: 5,
+      freq: 349.23
+    },
+    'T': {
+      key: 84,
+      index: 6,
+      freq: 369.99
+    },
+    'G': {
+      key: 71,
+      index: 7,
+      freq: 392.00
+    },
+    'Y': {
+      key: 89,
+      index: 8,
+      freq: 415.30
+    },
+    'H': {
+      key: 72,
+      index: 9,
+      freq: 440.00
+    },
+    'U': {
+      key: 85,
+      index: 10,
+      freq: 466.16
+    },
+    'J': {
+      key: 74,
+      index: 11,
+      freq: 493.88
+    },
+    'K': {
+      key: 75,
+      index: 12,
+      freq: 523.25
+    }
+  };
 
-var keysDown = [];
-function Keyboard() {
-}
+  var keysDown = [];
 
-Keyboard.prototype.keydown = function keydown(keyChar) {
-  if (keysDown.indexOf(keyChar.which) === -1) {
+  var keydown = function keydown(keyChar) {
+    if (keysDown.indexOf(keyChar.which) === -1) {
+      var char = String.fromCharCode(keyChar.which);
+      var key = keys[char];
+      if (key) {
+        oscillators[key.index].updateNote(key.freq);
+        oscillators[key.index].updateVolume(currentVolume);
+        keysDown.push(keyChar.which);
+        $("#key-" + char).addClass("keyon");
+      }
+    }
+  };
+
+  var keyup = function (keyChar) {
     var char = String.fromCharCode(keyChar.which);
     var key = keys[char];
-    if (key) {
-      oscillators[key.index].updateNote(key.freq);
-      oscillators[key.index].updateVolume(currentVolume);
-      keysDown.push(keyChar.which);
-      $("#key-" + char).addClass("keyon");
+    var keyIndex = keysDown.indexOf(keyChar.which);
+    if (keyIndex >= 0) {
+      oscillators[key.index].updateVolume(0);
+      delete keysDown[keyIndex];
+      $("#key-" + char).removeClass("keyon");
     }
+  };
+
+  var listen = function () {
+    var that = this;
+    $(document).keydown(function (e) {
+      that.keydown(e);
+    });
+
+    $(document).keyup(function (e) {
+      that.keyup(e);
+    });
+  };
+
+  return {
+    listen: listen,
+    keydown: keydown,
+    keyup: keyup
   }
-};
-
-Keyboard.prototype.keyup = function (keyChar) {
-  var char = String.fromCharCode(keyChar.which);
-  var key = keys[char];
-  var keyIndex = keysDown.indexOf(keyChar.which);
-  if (keyIndex >= 0) {
-    oscillators[key.index].updateVolume(0);
-    delete keysDown[keyIndex];
-    $("#key-" + char).removeClass("keyon");
-  }
-};
-
-Keyboard.prototype.listen = function () {
-  var that = this;
-  $(document).keydown(function (e) {
-    that.keydown(e);
-  });
-
-  $(document).keyup(function (e) {
-    that.keyup(e);
-  });
-};
+}();
