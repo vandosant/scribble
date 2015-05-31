1a5f95e850439a74e57ef8e71b9cf8cf761982d9
vandosant <sjskender@gmail.com>
2015-05-31 11:08:53 -0600

    OscillatorController should create oscillators

--- spec/javascripts/features/OscillatorControllerSpec.js
+++ spec/javascripts/features/OscillatorControllerSpec.js
@@ -4,4 +4,15 @@ describe("OscillatorController", function () {
 
     expect(typeof oscillator.initialize).toEqual("function");
   });
+
+  it("should create oscillators upon initialization", function() {
+    context = new AudioContext();
+
+    var oscillators = [];
+    var oscillator = OscillatorController({oscillators: oscillators, initialVolume: 0, initialFrequency: 261.63});
+
+    expect(oscillators.length).toBe(0);
+    oscillator.initialize();
+    expect(oscillators.length).toBeGreaterThan(0)
+  });
 });
