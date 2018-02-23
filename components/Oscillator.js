28f0bf2b4e86c57c5ad647bdfe18b6c6115ae574
vandosant <sjskender@gmail.com>
2018-02-22 23:34:34 -0700

    Fix audio api deprecations

--- components/Oscillator.js
+++ components/Oscillator.js
@@ -13,21 +13,21 @@ function oscillator(context, frequency, volume) {
     this.gainNode1.gain.value = this.volume;
     this.osc1.connect(this.gainNode1);
     this.osc1.type = "sine";
-    this.osc1.frequency.value = this.frequency;
+    this.osc1.frequency.setValueAtTime(this.frequency, context.currentTime);
     this.osc1.start(0);
 
     this.gainNode2.connect(this.context.destination);
     this.gainNode2.gain.value = this.volume;
     this.osc2.connect(this.gainNode2);
     this.osc2.type = "sine";
-    this.osc2.frequency.value = this.frequency / 2;
+    this.osc2.frequency.setValueAtTime(this.frequency / 2, context.currentTime);
     this.osc2.start(0);
 
     this.gainNode3.connect(this.context.destination);
     this.gainNode3.gain.value = this.volume * 0.8;
     this.osc3.connect(this.gainNode3);
     this.osc3.type = "triangle";
-    this.osc3.frequency.value = this.frequency / 2;
+    this.osc3.frequency.setValueAtTime(this.frequency / 2, context.currentTime);
     this.osc3.start(0);
   }
 
@@ -39,15 +39,15 @@ function oscillator(context, frequency, volume) {
 
   function updateVolume(value) {
     this.volume = value;
-    this.gainNode1.gain.setTargetAtTime(value, context.currentTime + 0.00001, 0.3);
-    this.gainNode2.gain.setTargetAtTime(value, context.currentTime + 0.00001, 0.3);
-    this.gainNode3.gain.setTargetAtTime(value * 0.8, context.currentTime + 0.00001, 0.3);
+    this.gainNode1.gain.setTargetAtTime(value, context.currentTime + 0.001, 0.3);
+    this.gainNode2.gain.setTargetAtTime(value, context.currentTime + 0.001, 0.3)
+    this.gainNode3.gain.setTargetAtTime(value * 0.8, context.currentTime + 0.001, 0.3)
   }
 
   function updateNote(value) {
-    this.osc1.frequency.value = (value);
-    this.osc2.frequency.value = (value) / 2;
-    this.osc3.frequency.value = (value) / 2;
+    this.osc1.frequency.setValueAtTime(value, context.currentTime);
+    this.osc2.frequency.setValueAtTime(value / 2, context.currentTime);
+    this.osc3.frequency.setValueAtTime(value / 2, context.currentTime);
   }
 
   function updateWave(id, newWave) {
