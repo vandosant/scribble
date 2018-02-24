cb18bb6c7eec32853bad15952122073b79207d71
vandosant <sjskender@gmail.com>
2018-02-23 22:16:14 -0700

    Add jest and update test

--- spec/javascripts/features/KeyboardSpec.js
+++ spec/javascripts/features/KeyboardSpec.js
@@ -1,51 +1,53 @@
-import keyboard from '../../../app/components/Keyboard.js';
+import assert from 'assert'
+import sinon from 'sinon'
+import keyboard from '../../../app/components/Keyboard.js'
 
-describe("keyboard", function () {
-  var keyboardInstance;
+describe('keyboard', function () {
+  var keyboardInstance
 
-  beforeEach(function () {
-    keyboardInstance = keyboard({volumeSelector: ".test-selector"});
-    keyboardInstance.initialize();
-  });
-
-  it("should listen for keydown events", function () {
-    spyOn(keyboardInstance, "keydown");
-    var keyDownEvent = jQuery.Event("keydown");
-    keyDownEvent.keyCode = 65;
-    $(document).trigger(keyDownEvent);
-
-    expect(keyboardInstance.keydown).toHaveBeenCalled();
-  });
-
-  it("should listen for keyup events", function () {
-    spyOn(keyboardInstance, "keyup");
-    var keyUpEvent = jQuery.Event("keyup");
-    keyUpEvent.keyCode = 65;
-    $(document).trigger(keyUpEvent);
-
-    expect(keyboardInstance.keyup).toHaveBeenCalled();
-  });
-
-  it("should set a default volume on creation", function () {
-    expect(keyboardInstance.volume).toEqual(.25);
-  });
-
-  it("should accept a volume on creation", function () {
-    keyboardInstance = keyboard({volume: .25});
-    expect(keyboardInstance.volume).toEqual(.25);
-  });
-
-  it("should accept a volume selector on creation", function () {
-    keyboardInstance = keyboard({volumeSelector: ".test-selector"});
-    expect(keyboardInstance.volumeSelector).toEqual(".test-selector");
-  });
-
-  it("should update the volume", function () {
-    var firstVolume = keyboardInstance.volume;
-
-    keyboardInstance.updateVolume(65);
-
-    var secondVolume = keyboardInstance.volume;
-    expect(secondVolume).toBeLessThan(firstVolume);
+  beforeEach(() => {
+    keyboardInstance = keyboard({volumeSelector: '.test-selector'})
+    keyboardInstance.initialize()
   })
-});
+
+  it('should listen for keydown events', function () {
+    sinon.spy(keyboardInstance, 'keydown')
+    var keyDownEvent = jQuery.Event('keydown')
+    keyDownEvent.keyCode = 65
+    $(document).trigger(keyDownEvent)
+
+    assert(keyboardInstance.keydown.calledOnce)
+  })
+
+  it('should listen for keyup events', function () {
+    spyOn(keyboardInstance, 'keyup')
+    var keyUpEvent = jQuery.Event('keyup')
+    keyUpEvent.keyCode = 65
+    $(document).trigger(keyUpEvent)
+
+    expect(keyboardInstance.keyup).toHaveBeenCalled()
+  })
+
+  it('should set a default volume on creation', function () {
+    expect(keyboardInstance.volume).toEqual(.25)
+  })
+
+  it('should accept a volume on creation', function () {
+    keyboardInstance = keyboard({volume: .25})
+    expect(keyboardInstance.volume).toEqual(.25)
+  })
+
+  it('should accept a volume selector on creation', function () {
+    keyboardInstance = keyboard({volumeSelector: '.test-selector'})
+    expect(keyboardInstance.volumeSelector).toEqual('.test-selector')
+  })
+
+  it('should update the volume', function () {
+    var firstVolume = keyboardInstance.volume
+
+    keyboardInstance.updateVolume(65)
+
+    var secondVolume = keyboardInstance.volume
+    assert.ok(secondVolume < firstVolume)
+  })
+})
