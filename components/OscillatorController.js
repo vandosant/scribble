296a3a760966239ebabfc872f8f7d7e84c2d4af1
vandosant <sjskender@gmail.com>
2018-03-06 21:44:56 -0700

    Refactoring

--- components/OscillatorController.js
+++ components/OscillatorController.js
@@ -1,25 +1,7 @@
 import context from './Context'
 import oscillator from './Oscillator'
 
-function oscillatorController (options) {
-  var oscillators = options['oscillators'] || []
-  var oscillatorSelector = options['oscillatorSelector'] || '.oscillator-wave'
-  var initialFrequency = options['initialFrequency'] || 261.63
-  var initialVolume = options['initialVolume'] || 0
-
-  var createOscillators = function () {
-    for (var i = 0; i < 13; i++) {
-      var osc = oscillator(context, initialFrequency, initialVolume)
-      oscillators.push(osc)
-    }
-  }
-
-  var connectOscillators = function () {
-    oscillators.forEach(function (osc) {
-      osc.connect()
-    })
-  }
-
+function oscillatorController ({ initialFrequency = 261.63, initialVolume = 0, oscillators = [] } = {}) {
   var updateWave = function (id, wave) {
     oscillators.forEach(function (osc) {
       osc.updateWave(id, wave)
@@ -27,12 +9,16 @@ function oscillatorController (options) {
   }
 
   var initialize = function () {
-    createOscillators()
-    connectOscillators()
+    for (var i = 0; i < 13; i++) {
+      var osc = oscillator(context, initialFrequency, initialVolume)
+      oscillators.push(osc)
+    }
+    oscillators.forEach(function (osc) {
+      osc.connect()
+    })
   }
 
   var setMode = function (options = []) {
-    var id = 1
     for (let i = 0; i < options.length; i++) {
       for (let j = 0; j < oscillators.length; j++) {
         oscillators[j].updateWave(i + 1, options[i])
