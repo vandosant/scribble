ad023398323edc23c7e3bc37a6fb0849d953a809
vandosant <sjskender@gmail.com>
2018-03-21 22:25:27 -0600

    Updating Visualizer

--- components/Visualizer.js
+++ components/Visualizer.js
@@ -1,71 +1,64 @@
 import context from './Context'
 
-var drawVisual
-var analyser = context.createAnalyser()
+let drawVisual
+let analyser = context.createAnalyser()
 
 function visualize () {
-  var canvas
-  var dest
+  let canvas
+  let dest
 
   function init (containerId, nodes) {
     canvas = document.getElementById(containerId)
     dest = context.createMediaStreamDestination()
-    var source = context.createMediaStreamSource(dest.stream)
-    var canvasCtx = canvas.getContext('2d')
-    var WIDTH = canvas.width
-    var HEIGHT = canvas.height
-    var visualSetting = 'sinewave'
+    const source = context.createMediaStreamSource(dest.stream)
+    const canvasCtx = canvas.getContext('2d')
+    const WIDTH = canvas.width
+    const HEIGHT = canvas.height
 
     source.connect(analyser)
-    for (var i = 0; i < nodes.length; i++) {
+    for (let i = 0; i < nodes.length; i++) {
       nodes[i].connect(dest)
     }
 
-    if (visualSetting == 'sinewave') {
-      analyser.fftSize = 2048
-      var bufferLength = analyser.frequencyBinCount // half the FFT value
-      var dataArray = new Uint8Array(bufferLength) // create an array to store the data
+    analyser.fftSize = 2048
+    const bufferLength = analyser.frequencyBinCount // half the FFT value
+    const dataArray = new Uint8Array(bufferLength) // create an array to store the data
 
-      canvasCtx.clearRect(0, 0, WIDTH, HEIGHT)
+    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT)
 
-      function draw () {
-        drawVisual = requestAnimationFrame(draw)
+    function draw () {
+      drawVisual = window.requestAnimationFrame(draw)
 
-        analyser.getByteTimeDomainData(dataArray) // get waveform data and put it into the array created above
+      analyser.getByteTimeDomainData(dataArray) // get waveform data and put it into the array created above
 
-        canvasCtx.fillStyle = 'rgb(200, 200, 200)' // draw wave with canvas
-        canvasCtx.fillRect(0, 0, WIDTH, HEIGHT)
+      canvasCtx.fillStyle = 'rgb(238, 238, 238)' // draw wave with canvas
+      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT)
 
-        canvasCtx.lineWidth = 2
-        canvasCtx.strokeStyle = 'rgb(0, 0, 0)'
+      canvasCtx.lineWidth = 2
+      canvasCtx.strokeStyle = 'rgb(0, 0, 0)'
 
-        canvasCtx.beginPath()
-        var sliceWidth = WIDTH * 1.0 / bufferLength
-        var x = 0
+      canvasCtx.beginPath()
+      const sliceWidth = WIDTH * 1.0 / bufferLength
+      let x = 0
 
-        for (var i = 0; i < bufferLength; i++) {
-          var v = dataArray[i] / 128.0
-          var y = v * HEIGHT / 2
+      for (var i = 0; i < bufferLength; i++) {
+        const v = dataArray[i] / 128.0
+        const y = v * HEIGHT / 2
 
-          if (i === 0) {
-            canvasCtx.moveTo(x, y)
-          } else {
-            canvasCtx.lineTo(x, y)
-          }
-
-          x += sliceWidth
+        if (i === 0) {
+          canvasCtx.moveTo(x, y)
+        } else {
+          canvasCtx.lineTo(x, y)
         }
 
-        canvasCtx.lineTo(canvas.width, canvas.height / 2)
-        canvasCtx.stroke()
+        x += sliceWidth
       }
 
-      draw()
-    } else if (visualSetting == 'off') {
-      canvasCtx.clearRect(0, 0, WIDTH, HEIGHT)
-      canvasCtx.fillStyle = 'red'
-      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT)
+      canvasCtx.lineTo(canvas.width, canvas.height / 2)
+      canvasCtx.stroke()
     }
+
+    draw()
   }
 
   function connect (node) {
@@ -73,8 +66,8 @@ function visualize () {
   }
 
   return {
-    init: init,
-    connect: connect
+    init,
+    connect
   }
 }
 
